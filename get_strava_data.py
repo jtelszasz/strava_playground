069eebed1835f77fef4e3a2cd16343a9c048d5f2
Justin Elszasz <jtelszasz@gmail.com>
2018-07-22 10:51:13 -0400

    API for activities

--- get_strava_data.py
+++ get_strava_data.py
@@ -1,2 +1,28 @@
 
 import config
+import urllib2
+import json
+import sqlite3
+
+def get_my_activities():
+    url = "https://www.strava.com/api/v3/athlete/activities?access_token=" + config.ACCESS_TOKEN
+
+    json_obj = urllib2.urlopen(url)
+
+    json_data = json.load(json_obj)
+
+    return json_data
+
+def send_to_sql():
+    '''send data to sqllite database'''
+
+
+def main():
+    json_data = get_my_activities()
+    for item in json_data:
+        print item["start_date_local"]
+        print item["distance"]
+
+
+if __name__ == "__main__":
+    main()
