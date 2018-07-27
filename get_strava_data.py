e66bd6bbd14920f2013237c41a04e85d0fc4c807
Justin Elszasz <jtelszasz@gmail.com>
2018-07-27 07:07:08 -0400

    update to python3, start sql

--- get_strava_data.py
+++ get_strava_data.py
@@ -1,28 +1,50 @@
 
 import config
-import urllib2
+from urllib.request import urlopen
 import json
 import sqlite3
+
+conn = sqlite3.connect("strava.db")
+c = conn.cursor()
 
 def get_my_activities():
     url = "https://www.strava.com/api/v3/athlete/activities?access_token=" + config.ACCESS_TOKEN
 
-    json_obj = urllib2.urlopen(url)
+    json_obj = urlopen(url)
 
     json_data = json.load(json_obj)
 
     return json_data
 
-def send_to_sql():
-    '''send data to sqllite database'''
+def create_activities_table(json_data):
+    cols = list(json_data[1].keys())
+    q = ('CREATE TABLE IF NOT EXISTS activities('
+         'start_date_local DATETIME '
+         'distance FLOAT'
+         ')')
 
+    #c.execute("sdf")
+
+def activities_to_sql():
+    '''insert into activities table'''
+    c.execute("INSERT INTO activities values(?, ?)")
+    c.commit()
 
 def main():
+    #create_activities_table()
+
     json_data = get_my_activities()
+
+
+
     for item in json_data:
-        print item["start_date_local"]
-        print item["distance"]
+        print(item["start_date_local"])
+        print(item["distance"])
+
+    return(json_data)
+
+
 
 
 if __name__ == "__main__":
-    main()
+    json_data = main()
