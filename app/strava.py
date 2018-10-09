0118ebcdf16915d6d5b6f17a525b609c445a3e23
Justin Elszasz <jtelszasz@gmail.com>
2018-10-08 22:19:26 -0400

    delete db stuff for now

--- app/strava.py
+++ app/strava.py
@@ -5,9 +5,6 @@ import ssl
 
 from app import app
 
-conn = sqlite3.connect("strava.db")
-c = conn.cursor()
-
 def get_my_activities():
     context = ssl._create_unverified_context()
     url = "https://www.strava.com/api/v3/athlete/activities?access_token=" + app.config['STRAVA_TOKEN']
@@ -17,8 +14,3 @@ def get_my_activities():
     json_data = json.load(json_obj)
 
     return json_data
-
-data = get_my_activities()
-
-conn.commit()
-conn.close()
