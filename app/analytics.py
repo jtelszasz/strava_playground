4d745e95835897a1a21f72d703f294388cd4d985
Justin Elszasz <jtelszasz@gmail.com>
2018-11-01 07:02:20 -0400

    loop through pages in api

--- app/analytics.py
+++ app/analytics.py
@@ -8,20 +8,29 @@ from app import app
 def get_my_activities_json():
 
     context = ssl._create_unverified_context()
-    per_page = 100
+    per_page = 200 # max per Strava API docs
     page = 1
 
     query_dict = { "access_token" : app.config["STRAVA_TOKEN"],
                    "per_page" : per_page,
                    "page" : page}
+
     url = "https://www.strava.com/api/v3/athlete/activities"
 
-    response = requests.get(url, params=query_dict).json()
-    json_data = response
-    # old way
-    #json_obj = urlopen(url, context = context)
-    #json_data = json.load(json_obj)
 
+    json_data = []
+
+    while True:
+        request = requests.get(url, params=query_dict).json()
+
+        if len(request) == 0:
+            break
+
+        for activity in request:
+            json_data.append(activity)
+
+
+        query_dict["page"] += 1
     return json_data
 
 def create_activities_table(cursor):
